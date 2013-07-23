1041c3e692208b1e7fa2e554dbe250b71b710253
Jason Maldonis <jjmaldonis@gmail.com>
2013-07-23 10:48:25 -0500

    Removing unwanted files from github repo.

--- 060911_rmc_eam_gr_vk_t4/ran2__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Fri Jul 12 09:33:38 2013
-        MODULE RAN2__genmod
-          INTERFACE 
-            FUNCTION RAN2(IDUM)
-              INTEGER(KIND=4) :: IDUM
-              REAL(KIND=4) :: RAN2
-            END FUNCTION RAN2
-          END INTERFACE 
-        END MODULE RAN2__genmod
