a9a2452083645cb5af7bb0be951d6083d47cf2cf
Jason Maldonis <jjmaldonis@gmail.com>
2013-07-30 09:24:10 -0500

    Adding md femsim files - excluding .xyz files but these are reproducable. Read the readme.

---
+++ md_dump_to_Vk_conversion/npt_heat/dump_to_xyz.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+def main():
+    dirs = [ name for name in os.listdir(os.getcwd()) if os.path.isdir(os.path.join(os.getcwd(), name)) ]
+    for name in dirs:
+        parse_dump(name)
+
+
+
+def parse_dump(dirname):
+    df = open( dirname + '/' + dirname + '_npt_heat.dump', 'r' )
+    xyz = None
+    line = df.readline()
+    while(len(line)!= 0):
+        # If we get to a new timestep create a new file and close the old one.
+        if( line == "ITEM: TIMESTEP\n" ):
+            if(xyz != None):
+                xyz.write("-1")
+                xyz.close()
+            timestep = int(df.readline())
+            line = df.readline() # ITEM: NUMBER OF ATOMS
+            natoms = int(df.readline())
+            line = df.readline() # ITEM: BOX BOUNDS pp pp pp
+            xsize = sum( [ abs(float(x)) for x in df.readline().split(" ") ] )
+            ysize = sum( [ abs(float(x)) for x in df.readline().split(" ") ] )
+            zsize = sum( [ abs(float(x)) for x in df.readline().split(" ") ] )
+            line = df.readline() # ITEM: ATOMS id type xs ys zs
+            xyz = open( dirname + '/xyz_files/' + dirname + '_npt_heat_'+str(timestep)+'.xyz', 'w')
+            xyz.write( str(natoms) + " atoms !Comment line\n")
+            xyz.write( str(xsize) + " " + str(ysize) + " " + str(zsize) + " !Box size\n" )
+            linenum = 9
+        else:
+            # We have an atom. Save it.
+            # The types given in the dump file need to be converted to the actual elements' atomic numbers. In my case, they are all the same, so I will not code the universal way to convert them. You can find the masses for the types in the .dat files and then look up which mass is which element. For my case, 1 = 91.22 = Zr = 40, 2 = 63.55 = Cu = 29, and 3 = 26.98 = Al = 13.
+            split_line = line.split(" ")[1:]
+            if(split_line[0] == "1"):
+                split_line[0] = "40"
+            elif(split_line[0] == "2"):
+                split_line[0] = "29"
+            elif(split_line[0] == "3"):
+                split_line[0] = "13"
+            xyz.write( " ".join( split_line) )
+        line = df.readline()
+
+
+if __name__ == "__main__":
+    main();
---
+++ md_dump_to_Vk_conversion/npt_heat/xyz_to_femsim.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+def main():
+    dirs = [ name for name in os.listdir(os.getcwd()) if os.path.isdir(os.path.join(os.getcwd(), name)) ]
+    for dir in dirs[1:2]:
+        print dir
+        for xyz in os.listdir(dir + '/' + 'xyz_files'):
+            run_femsim(xyz, dir)
+
+
+
+def run_femsim(xyz, dir):
+    timestep = xyz[xyz.find('1'):-4]
+    if timestep[0:12] == '10_npt_heat_':
+        timestep = timestep[12:]
+    #print 'Running femsim on ' + dir+'/xyz_files/'+dir+'_npt_heat_'+timestep+'.xyz '
+    os.system('qsub submit.sh "./rmc_for_md_sims ' + dir+'/xyz_files/'+dir+'_npt_heat_'+timestep+'.xyz ' + dir+'/param_file.in ' + dir+'/vk/vk_'+timestep+'.out"')
+    
+if __name__ == "__main__":
+    main();
+
